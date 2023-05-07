b69614de43c42900aaa5b2bb64fa9e36aa2c89a5
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-06 23:42:00 -0600

    Agregué el componente de Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LogoutButton from './LogoutButton'
+import '../Styles/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <LogoutButton />
+    </div>
+  )
+}
+
+export default Header
