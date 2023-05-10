4e6bb9b73d2fe72e9efdd285e9d5f619a10f077b
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-09 23:38:23 -0600

    Agregué el componente Header y sus estilos

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import LogoutButton from './LogoutButton'
+import Schedule from './Schedule'
 import '../Styles/Header.css'
 
 const Header = () => {
   return (
     <div className='header'>
+      <div className='logo_container'>Fantasy VCT Americas</div>
+      <Schedule />
       <LogoutButton />
     </div>
   )
